bff4edbb273fa5a2e03d14bab44c9ada25daba38
icebear0815 <lars.schindler@labema.de>
2020-09-19 16:14:00 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,11 @@ let paddleB: game.LedSprite = null
 let paddleA: game.LedSprite = null
 paddleA = game.createSprite(2, 4)
 paddleB = game.createSprite(3, 4)
+let ball = game.createSprite(randint(0, 4), 0)
+let directionX = randint(-1, 1)
+let directionY = 1
+basic.pause(500)
 basic.forever(function () {
-	
+    ball.change(LedSpriteProperty.X, directionX)
+    ball.change(LedSpriteProperty.Y, directionY)
 })
